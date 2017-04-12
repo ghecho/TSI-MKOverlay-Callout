27336a017ab5610e87acc3d74786865563ebc950
Diego <ghecho@gmail.com>
2017-04-11 20:15:41 -0500

    Initial Commit

---
+++ TSI-MKOverlay-Callout/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TSI-MKOverlay-Callout
+//
+//  Created by Diego on 4/11/17.
+//  Copyright © 2017 Diego. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+

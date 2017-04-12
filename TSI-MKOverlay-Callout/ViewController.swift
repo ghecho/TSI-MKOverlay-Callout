f0f0a27ae4a9a420ee63753cd70c8f2d4aecbe10
Diego <ghecho@gmail.com>
2017-04-11 21:03:54 -0500

    initial commit

--- TSI-MKOverlay-Callout/ViewController.swift
+++ TSI-MKOverlay-Callout/ViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import Cocoa
+import MapKit
 
-class ViewController: NSViewController {
+extension MKPolygon
+{
+    func contains(coordinate: CLLocationCoordinate2D) -> Bool
+    {
+        let polygonRenderer = MKPolygonRenderer(polygon: self)
+        let currentMapPoint: MKMapPoint = MKMapPointForCoordinate(coordinate)
+        let polygonViewPoint: CGPoint = polygonRenderer.point(for: currentMapPoint)
+        return polygonRenderer.path.contains(polygonViewPoint)
+    }
+}
+
+struct MyFigure
+{
+    var name: String
+    var coordinates: [CLLocationCoordinate2D]
+    var polygon: MKPolygon
+}
+
+class ViewController: NSViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var figures = [MyFigure]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        mapView.delegate = self
+        
+        mapView.region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 19.445382956560934, longitude: -99.117824745970992), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        figures = Utils.getFigures()
+        mapView.addOverlays(figures.map{$0.polygon})
     }
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer
+    {
+        if let polygon = overlay as? MKPolygon
+        {
+            let renderer = MKPolygonRenderer(polygon: polygon)
+            renderer.lineWidth = 1.0
+            renderer.strokeColor = NSColor.red
+            renderer.fillColor = NSColor(colorLiteralRed: 1.0, green: 0.0, blue: 0.0, alpha: 0.1)
+            return renderer
+        }
+        return MKOverlayRenderer(overlay: overlay)
     }
 
 
